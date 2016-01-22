d5b534bffc34608fe9b4dd3b5f57bdb2ac9278b2
Ann Ho <kjuri27@yahoo.com>
2016-01-22 01:52:52 -0500

    I finally made the camera follow with 1.5 m square

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,14 +3,38 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
     public GameObject player;
+	public float cameraSpeed;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+	Vector3 camPos;
+	Vector3 playerPos;
+
 	void Update () {
-        gameObject.transform.position = player.transform.position;
+		// camPos + playerPos are just shortcuts
+		camPos = gameObject.transform.position;
+		playerPos = player.transform.position;
+
+		// Position of camera after checks
+		Vector3 newPos = camPos;
+
+		// Set up new position of camera
+		// If player is 1.5 meters left of the center
+		if (camPos.x - playerPos.x > 1.5f) {
+			newPos.x = playerPos.x + 1.5f;
+		}
+		// If player is 1.5 meters right of the center
+		if (camPos.x - playerPos.x < -1.5f) {
+			newPos.x = playerPos.x - 1.5f;
+		}
+		// If player is 1.5 meters below center
+		if (camPos.z - playerPos.z > 1.5f) {
+			newPos.z = playerPos.z + 1.5f;
+		}
+		// If player is 1.5 meters above center
+		if (camPos.z - playerPos.z < -1.5f) {
+			newPos.z = playerPos.z - 1.5f;
+		}
+
+		// Move camera
+		gameObject.transform.position = newPos;
 	}
 }
